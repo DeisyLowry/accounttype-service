dde912bfd8f33c3e399f51c0c8dd8c2deec05a27
David Lowry <DavidLowry64@gmail.com>
2023-08-08 13:49:58 -0400

    Initial Commit

---
+++ src/main/java/com/ttrc/accounttype/AccounttypeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ttrc.accounttype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccounttypeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccounttypeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttrc/accounttype/AccounttypeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttrc.accounttype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccounttypeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
